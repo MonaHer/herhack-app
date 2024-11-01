8c25b97bd366ff3066a883a0e8ad0d7fecb6c38f
MonaHer <hertermona@gmail.com>
2024-11-01 16:21:48 +0100

    add json response

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+// Home.js
+import React from "react";
+import { Typography } from "@mui/material";
+
+function Home() {
+  return <Typography variant="h4">Welcome to the Home Page</Typography>;
+}
+
+export default Home;
